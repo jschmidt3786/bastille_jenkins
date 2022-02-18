3fa37e794f5248a7715a3615723625f002ef9630
Jeff Schmidt <jeff.schmidt@zenith.com>
2022-02-18 15:45:19 -0600

    leverage the vhost template for user input of FQDN

--- root/script.sh
+++ root/script.sh
@@ -4,12 +4,16 @@ mkdir -p /usr/local/etc/nginx/tls
 cd /usr/local/etc/nginx/tls/ || exit
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout jenkins.key -out jenkins.crt
 
+printf "FQDN for vhost? (and optionally for Let's Encrypt) " >&2
+read -r FQDN
+sed -i '/usr/local/etc/nginx/conf.d/jenkins.conf.sample' s/FQDN/"$FQDN"/ /usr/local/etc/nginx/conf.d/jenkins.conf
+diff -u /usr/local/etc/nginx/conf.d/jenkins.conf.sample /usr/local/etc/nginx/conf.d/jenkins.conf
+echo
+
 printf "attempt adding a Let's Encrypt signed keypair? (anything but 'y' cancels) " >&2
 read -r option
 if [ "$option" = "y" ]; then
   pkg install -y security/py-certbot security/py-certbot-nginx
-  printf "FQDN to register? " >&2
-  read -r FQDN
   certbot --nginx -d "$FQDN"
   sysrc -f /etc/periodic.conf weekly_certbot_enable="YES"
 fi
